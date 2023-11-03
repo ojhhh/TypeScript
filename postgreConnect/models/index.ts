1c0f3377c41169df965bc87bf05fdd9ed2d44066
ojhhh <winnel@naver.com>
2023-11-03 15:59:45 +0900

    postgresql 청약 테이블 생성 완료

--- postgreConnect/models/index.ts
+++ postgreConnect/models/index.ts
@@ -3,11 +3,15 @@ import { config } from "../config";
 import { Sequelize } from "sequelize";
 import Connect from "./connect";
 import { Users, userInitModel } from "./users";
+import Subscriptions from "./subscriptions";
+import Subscriptions_own from "./subscriptions_own";
 
 interface DB {
   sequelize: Sequelize;
   Connect: typeof Connect;
   Users: typeof Users;
+  Subscriptions: typeof Subscriptions;
+  Subscriptions_own: typeof Subscriptions_own;
 }
 
 const sequelize = new Sequelize(
@@ -23,9 +27,13 @@ const db: DB = {
   sequelize,
   Connect,
   Users,
+  Subscriptions,
+  Subscriptions_own,
 };
 
 Connect.initModel(sequelize);
 userInitModel(sequelize);
+Subscriptions.initModel(sequelize);
+Subscriptions_own.initModel(sequelize);
 
 export default db;
